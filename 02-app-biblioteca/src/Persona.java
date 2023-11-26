e51c5c50b31c0f25fb90e79f18586c68deca0acd
juanma47z <141055534+juanma47z@users.noreply.github.com>
2023-11-26 15:26:28 -0300

    Add files via upload

---
+++ 02-app-biblioteca/src/Persona.java
@@ -0,0 +1,68 @@
+public class Persona {
+  private String nombre;
+    private String apellido;
+    private String dni;
+    private String email;
+    private String telefono;
+    private String contrasena;
+    //private String nombreUsuario;
+
+  public Persona(String nombre, String apellido, String dni, String email, String telefono, String contrasena) {
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.dni = dni;
+    this.email = email;
+    this.telefono = telefono;
+    this.contrasena = contrasena;
+  }
+
+  public String getNombre() {
+    return nombre;
+  }
+
+  public void setNombre(String nombre) {
+    this.nombre = nombre;
+  }
+
+  public String getApellido() {
+    return apellido;
+  }
+
+  public void setApellido(String apellido) {
+    this.apellido = apellido;
+  }
+
+  public String getDni() {
+    return dni;
+  }
+
+  public void setDni(String dni) {
+    this.dni = dni;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getTelefono() {
+    return telefono;
+  }
+
+  public void setTelefono(String telefono) {
+    this.telefono = telefono;
+  }
+
+  public String getContrasena() {
+    return contrasena;
+  }
+
+  public void setContrasena(String contrasena) {
+    this.contrasena = contrasena;
+  }
+    
+    
+}
